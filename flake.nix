779f1cdd2f0f7577f3e0f8aefd28a36c38de2053
Peter A. <ink.splatters@pm.me>
2024-06-01 20:01:29 +0200

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,87 @@
 {
-  outputs = { self, nixpkgs, flake-utils, }:
-    flake-utils.lib.eachDefaultSystem (system:
+  description = "Swift Format";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        gitignore.follows = "gitignore";
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs";
+      };
+    };
+    gitignore = {
+      url = "github:hercules-ci/gitignore";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cachix.cachix.org"
+      "https://cache.nixos.org"
+    ];
+    extra-trusted-public-keys = [
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "aarch64-darwin.cachix.org-1:mEz8A1jcJveehs/ZbZUEjXZ65Aukk9bg2kmb0zL9XDA="
+    ];
+  };
+
+  outputs =
+    {
+      pre-commit-hooks,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell.override { stdenv = pkgs.swift.stdenv; } {
-          buildInputs = with pkgs;[
-            swift
-            swiftpm
-            swiftPackages.Foundation
-            darwin.apple_sdk.frameworks.AppKit
-          ];
-
-          # https://github.com/NixOS/nix/issues/6677
-          shellHook = ''
-            export PS1='\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] '
-          '';
+        pre-commit-check = pkgs.callPackage ./pre-commit-check.nix {
+          inherit pkgs pre-commit-hooks system;
         };
-      });
+      in
+      with pkgs;
+      {
+        checks = {
+          inherit pre-commit-check;
+        };
+
+        formatter = nixfmt-rfc-style;
+        devShells = {
+          default = mkShell.override { inherit (swift) stdenv; } {
+            nativeBuildInputs = with pkgs; [
+              swift
+              swiftpm
+              xcodebuild
+            ];
+
+            shellHook =
+              pre-commit-check.shellHook
+              + ''
+                export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
+              '';
+          };
+          install-hooks = mkShell.override { stdenv = stdenvNoCC; } {
+            inherit system;
+            shellHook =
+              let
+                inherit (pre-commit-check) shellHook;
+              in
+              ''
+                ${shellHook}
+                echo Done!
+                exit
+              '';
+          };
+        };
+      }
+    );
 }
